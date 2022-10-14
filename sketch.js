8a3dcc9b0ca8604adebc15164e03051a5a88812c
Simonluca Pisano <43146469+simonlucapisano@users.noreply.github.com>
2022-10-14 02:03:27 +0200

    margin rect

--- sketch.js
+++ sketch.js
@@ -1,8 +1,12 @@
+var dist = 10;
+
 function setup() {
   createCanvas(600, 600);
 }
 
 function draw() {
-  background(51);
-
+  background(255);
+  fill(151);
+  noStroke();
+  rect(dist, dist, width-(2*dist),50 , 20);
 }
