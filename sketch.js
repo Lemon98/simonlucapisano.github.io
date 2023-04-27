87ec0fb1fa0dda35655b2739299eeb6a217035d1
Simonluca Pisano <43146469+simonlucapisano@users.noreply.github.com>
2023-04-27 14:29:11 +0200

    font change

---
+++ sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    fullscreen();
+}
+  
+  function draw() {
+    noFill();
+    strokeWeight(4);
+    stroke(255);
+    ellipse(width/2, height/2, 200, 200);
+    
+  }
